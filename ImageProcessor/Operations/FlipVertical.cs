43fb5981ceb34dd80af20e4d3b25216f06f182b8
khometit <averydinh1996@gmail.com>
2022-02-21 10:44:35 -0800

    Initial commit

---
+++ ImageProcessor/Operations/FlipVertical.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ImageMagick;
+
+namespace ImageProcessor.Operations
+{
+    public class FlipVertical : IOperation
+    {
+        public FlipVertical()
+        {
+        }
+
+        public void PerformOperation(byte[] source)
+        {
+            string testSource = "./inputs/niel.jpeg";
+
+            var image = new MagickImage(testSource);
+            image.Flop();
+
+            image.Write("flopped.png");
+
+        }
+    }
+}
---
+++ ImageProcessor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using ImageProcessor.Operations;
+
+namespace ImageProcessor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IOperation operation = new Resize();
+            operation.PerformOperation(null);
+            CreateHostBuilder(args).Build().Run();
+            
+        }
+
+        //public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        //{
+        //    using (var ms = new MemoryStream())
+        //    {
+        //        imageIn.Save(ms, imageIn.RawFormat);
+        //        return ms.ToArray();
+        //    }
+        //}
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
